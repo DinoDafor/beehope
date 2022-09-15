9e7129697c5886e9cd552b9a08282d39bc5c3d13
DinoDafor <dinodafor@mail.ru>
2022-09-15 15:15:37 +0300

    Added post and get photos

--- src/main/java/com/example/beehope/controller/RequestController.java
+++ src/main/java/com/example/beehope/controller/RequestController.java
@@ -6,14 +6,27 @@ import com.example.beehope.mapper.RequestMapper;
 import com.example.beehope.model.Request;
 import com.example.beehope.service.RequestService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 
 
 import javax.validation.Valid;
 import javax.validation.constraints.Min;
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
 import java.util.NoSuchElementException;
 
@@ -68,6 +81,37 @@ public class RequestController {
                 .orElseThrow(ThereIsNoSuchRequestException::new);
         requestService.delete(request.getId());
         return ResponseEntity.ok().body("Request with ID : " + id + " deleted with success!");
+    }
+
+
+    @PostMapping("/upload")
+    public ResponseEntity uploadToLocalFileSystem(@RequestParam("file") MultipartFile file) {
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+        Path path = Paths.get(fileName);
+        try {
+            Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/files/download/")
+                .path(fileName)
+                .toUriString();
+        return ResponseEntity.ok(fileDownloadUri);
+    }
+    @GetMapping("/download/{fileName:.+}")
+    public ResponseEntity downloadFileFromLocal(@PathVariable String fileName) {
+        Path path = Paths.get( fileName);
+        Resource resource = null;
+        try {
+            resource = new UrlResource(path.toUri());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return ResponseEntity.ok()
+                .contentType(MediaType.IMAGE_PNG)
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
     }
 
 }
